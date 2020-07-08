2d6b48a7142531cc11063c170026bc638442a020
F2Ville <feliciendeville@gmail.com>
2020-07-08 19:08:56 +0200

    ⏱Changing Timezone offset
    Due to a problem with my local timezone, i changed the offset of the clock to match with mine.
    So I removed it because the problem is fixed.

--- clock.js
+++ clock.js
@@ -25,35 +25,11 @@ function showPhysic() {
     document.body.style.backgroundColor = '#457b9d'
 }
 
-function setHourGood(hour) {
-    if (hour < 19) {
-        return hour + 5
-    }
-    else if (hour == 19) {
-        return 0
-    }
-    else if (hour == 20) {
-        return 1
-    }
-    else if (hour == 21) {
-        return 2
-    }
-    else if (hour == 22) {
-        return 3
-    }
-    else if (hour == 23) {
-        return 4
-    }
-    else if (hour == 24 || hour == 0 || hour == 00) {
-        return 5
-    }
-}
-
 setInterval(() => {
 
     let deg = 6
     let date = new Date()
-    let hoursT = setHourGood(date.getHours()) * 30
+    let hoursT = date.getHours() * 30
     let minutesT = date.getMinutes() * deg
     let secondsT = date.getSeconds() * deg
 
@@ -66,7 +42,7 @@ setInterval(() => {
 setInterval(() => {
 
     let date = new Date()
-    let hoursT = ("0" + setHourGood(date.getHours())).slice(-2)
+    let hoursT = ("0" + date.getHours()).slice(-2)
     let minutesT = ("0" + date.getMinutes()).slice(-2)
     let secondsT = ("0" + date.getSeconds()).slice(-2)
 
